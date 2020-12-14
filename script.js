cfca57fac8170c88236170f6c486e602c1a9ad5c
Menuka Gusain <menukagusain@gmail.com>
2020-12-13 21:21:34 -0500

    added local storage

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 // This will display current date
-$("#currentDay").text(new Date());
+var currentTime = $("#currentDay").text(new Date());
 
-// this will save text to local storage 
+// Assigning variable to html text id
+
+var A9Am=$("#me9");
+var A10Am=$("#me10");
+var A9Am=$("#me11");
+var A10Am=$("#me12");
+var A9Am=$("#me13");
+var A10Am=$("#me14");
+var A9Am=$("#me15");
+var A10Am=$("#me16");
+var A9Am=$("#me17");
+
 
 $("button").on("click", function()
-
 {
-  localStorage.setItem("boxvalue", document.querySelector('.description').value);
-console.log("this");
- });
+localStorage.setItem("9AM", document.querySelector('#me9').value)
+localStorage.setItem("10AM", document.querySelector('#me10').value)
+localStorage.setItem("11AM", document.querySelector('#me11').value)
+localStorage.setItem("12PM", document.querySelector('#me12').value)
+localStorage.setItem("1PM", document.querySelector('#me13').value)
+localStorage.setItem("2PM", document.querySelector('#me14').value)
+localStorage.setItem("3PM", document.querySelector('#me15').value)
+localStorage.setItem("4PM", document.querySelector('#me16').value)
+localStorage.setItem("5PM", document.querySelector('#me17').value)
+
+if(currentTime=A9Am.value){
+
+
+}
+
+});
 
 
 
